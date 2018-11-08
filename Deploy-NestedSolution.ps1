3a0aff8b70e6747b1b38049ef4f22da38112e17c
unattend-dk <36849067+unattend-dk@users.noreply.github.com>
2018-11-08 16:14:50 +0100

    Add files via upload

---
+++ Deploy-NestedSolution.ps1
@@ -0,0 +1,35 @@
+﻿### Define variables
+{
+
+$location = 'Australia Southeast'
+$resourceGroupName = 'contoso-arm-nested'
+$resourceDeploymentSolutionName = 'contoso-arm-nested-deployment'
+$templateBasePath = $env:SystemDrive + '\' + 'pluralsight' + '\' + 'nested'
+$templateFile = 'azureDeploy.json'
+$template = $templateBasePath + '\' + $templateFile
+$templateParametersFile = 'azureDeploy.parameters.json'
+$templateParameters = $templateBasePath + '\' + $templateParametersFile
+
+}
+
+### Create Resource Group
+{
+
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $Location `
+    -Verbose -Force
+
+}
+
+### Deploy IaaS Solution
+{
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentSolutionName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -TemplateParameterFile $templateParameters `
+    -Verbose -Force
+
+}
